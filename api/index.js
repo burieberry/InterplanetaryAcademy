e8a99d1c27965cd811737c5287d4e0c35266f608
Burcu Noyan <burcunoyan@gmail.com>
2017-09-24 21:43:46 -0400

    get requests

--- api/index.js
+++ api/index.js
@@ -10,4 +10,22 @@ app.get('/campuses', (req, res, next) => {
     .catch(next)
 });
 
+app.get('/campuses/:id', (req, res, next) => {
+  Campus.findById(req.params)
+    .then(campus => res.send(campus))
+    .catch(next)
+});
+
+app.get('/students', (req, res, next) => {
+  Student.findAll()
+    .then(students => res.send(students))
+    .catch(next)
+});
+
+app.get('/students/:id', (req, res, next) => {
+  Campus.findById(req.params)
+    .then(student => res.send(student))
+    .catch(next)
+});
+
 module.exports = app;
