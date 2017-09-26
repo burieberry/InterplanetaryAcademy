9c689813341542a90dc02bad4fdfc667176c71a4
Burcu Noyan <burcunoyan@gmail.com>
2017-09-26 00:26:14 -0400

    logging false

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/campus');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/campus', { logging: false });
 
 module.exports = conn;
