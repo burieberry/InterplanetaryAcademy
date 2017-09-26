abb468ff0a6f7d31578ed5392da5b0c00135329b
Burcu Noyan <burcunoyan@gmail.com>
2017-09-25 23:40:36 -0400

    redirect /api landing page to be /api/campuses

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const app = require('express').Router();
 
+app.get('/', (req, res, next) => res.redirect('/api/campuses'));
 app.use('/campuses', require('./campuses'));
 app.use('/students', require('./students'));
 
