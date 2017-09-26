71f3f715fc6d0acea53c6964184929143bf9850b
Burcu Noyan <burcunoyan@gmail.com>
2017-09-25 21:48:49 -0400

    all router tests pass

--- test/testSpecs.js
+++ test/testSpecs.js
@@ -3,29 +3,29 @@ const expect = require('chai').expect;
 const supertest = require('supertest');
 const app = supertest.agent(require('../server'));
 
-describe('GET /', function() {
-  it('responds with 200', function() {
+describe('GET /', () => {
+  it('responds with 200', () => {
     app.get('/')
       .expect(200);
   });
 });
 
-describe('GET /api/campuses', function() {
-  it('responds with 200', function() {
+describe('GET /api/campuses', () => {
+  it('responds with 200', () => {
     return app.get('/api/campuses')
       .expect(200);
   });
 
-  it('should include Campus 2', function() {
+  it('should include Campus 2', () => {
     return app.get('/api/campuses')
-      .expect(function(res) {
+      .expect(res => {
         expect(res.text).to.contain('Campus 2');
       })
   })
 });
 
-describe('GET /api/campuses/1', function() {
-  it('gets campus with id 1', function() {
+describe('GET /api/campuses/1', () => {
+  it('gets campus with id 1', () => {
     return app.get('/api/campuses/1')
       .expect(res => {
         var campusName = res.body.name;
@@ -34,22 +34,22 @@ describe('GET /api/campuses/1', function() {
   });
 });
 
-describe('GET /api/students', function() {
-  it('responds with 200', function() {
+describe('GET /api/students', () => {
+  it('responds with 200', () => {
     return app.get('/api/students')
       .expect(200);
   });
 
-  it('should include Mikasa', function() {
+  it('should include Mikasa', () => {
     return app.get('/api/students')
-      .expect(function(res) {
+      .expect(res => {
         expect(res.text).to.contain('Mikasa');
       })
   })
 });
 
-describe('GET /api/students/1', function() {
-  it('gets student with id 1', function() {
+describe('GET /api/students/1', () => {
+  it('gets student with id 1', () => {
     return app.get('/api/students/1')
       .expect(res => {
         var studentName = res.body.name;
@@ -60,7 +60,7 @@ describe('GET /api/students/1', function() {
 
 describe('POST /api/campuses', () => {
   it('returns the new campus', () => {
-    const campus = { name: 'Campus New' };
+    const campus = { id: 777, name: 'Campus 777' };
     return app.post('/api/campuses')
       .send(campus)
       .expect(201)
@@ -72,7 +72,7 @@ describe('POST /api/campuses', () => {
 
 describe('POST /api/students', () => {
   it('returns the new student', () => {
-    const student = { name: 'Sasha', campusId: 2 };
+    const student = { id: 777, name: 'Sasha', campusId: 2 };
     return app.post('/api/students')
       .send(student)
       .expect(201)
@@ -84,7 +84,7 @@ describe('POST /api/students', () => {
 
 describe('PUT /api/campuses/:id', () => {
   it('can update campus name', () => {
-    const campus = { id: 6, name: 'Campus V3' };
+    const campus = { id: 777, name: 'Campus 888' };
     return app.put(`/api/campuses/${campus.id}`)
       .send(campus)
       .expect(200)
@@ -96,7 +96,7 @@ describe('PUT /api/campuses/:id', () => {
 
 describe('PUT /api/students/:id', () => {
   it('can update student name', () => {
-    const student = { id: 6, name: 'Commander' };
+    const student = { id: 777, name: 'Sashir' };
     return app.put(`/api/students/${student.id}`)
       .send(student)
       .expect(200)
@@ -106,7 +106,7 @@ describe('PUT /api/students/:id', () => {
   });
 
   it('can update student email', () => {
-    const student = { id: 7, email: 'sasha@mh.com' };
+    const student = { id: 777, email: 'sashir@mh.com' };
     return app.put(`/api/students/${student.id}`)
       .send(student)
       .expect(200)
@@ -116,7 +116,7 @@ describe('PUT /api/students/:id', () => {
   });
 
   it('can update student name and email', () => {
-    const student = { id: 8, name: 'Zate', email: 'zate@mh.com' };
+    const student = { id: 777, name: 'Zate', email: 'zate@mh.com' };
     return app.put(`/api/students/${student.id}`)
       .send(student)
       .expect(200)
@@ -127,7 +127,7 @@ describe('PUT /api/students/:id', () => {
   });
 
   it('can update student campus id', () => {
-    const student = { id: 9, name: 'Guillome', email: 'guillome@mh.com', campusId: 4 };
+    const student = { id: 777, name: 'Guillome', email: 'guillome@mh.com', campusId: 4 };
     return app.put(`/api/students/${student.id}`)
       .send(student)
       .expect(200)
@@ -139,7 +139,7 @@ describe('PUT /api/students/:id', () => {
 
 describe('DELETE /api/campuses/:id', () => {
   it('returns 204', () => {
-    const campus = { id: 12 };
+    const campus = { id: 777 };
     return app.delete(`/api/campuses/${campus.id}`)
       .expect(204)
   });
@@ -164,7 +164,7 @@ describe('DELETE /api/campuses/:id', () => {
 
 describe('DELETE /api/students/:id', () => {
   it('returns 204', () => {
-    const student = { id: 10 };
+    const student = { id: 777 };
     return app.delete(`/api/students/${student.id}`)
       .expect(204)
   });
