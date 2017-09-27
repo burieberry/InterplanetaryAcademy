ddf7a1bf35c0fc39798a95139725f13fecd6c380
Burcu Noyan <burcunoyan@gmail.com>
2017-09-27 12:17:15 -0400

    make table

--- src/Components/Students.js
+++ src/Components/Students.js
@@ -2,30 +2,36 @@ import React from 'react';
 
 const Students = ({ students, campuses }) => {
   return (
-    <ul className="col-xs-10 list-group">
-      {
-        students.map(student => {
-          return (
-            <li key={ student.id } className="list-group-item">
-              <ul className="list-unstyled">
-                <li>
-                  <strong>{ student.name }</strong>
-                </li>
-                <li>
-                  <a href={`mailTo:${ student.email }`}>{ student.email }</a>
-                </li>
-                <li>{
-                  campuses.filter(campus => campus.id === student.campusId).length ?
+    <section className="col-xs-10">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Campus</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          students.map(student => {
+            return (
+              <tr key={ student.id }>
+                <td><strong>{ student.name }</strong></td>
+                <td><a href={`mailTo:${ student.email }`}>{ student.email }</a></td>
+                <td>
+                  {
+                    campuses.filter(campus => campus.id === student.campusId).length ?
                     campuses.filter(campus => campus.id === student.campusId)[0].name
-                    : null
-                }</li>
-              </ul>
-
-            </li>
-          )
-        })
-      }
-    </ul>
+                    : '-'
+                  }
+                </td>
+              </tr>
+            )
+          })
+        }
+        </tbody>
+      </table>
+    </section>
   );
 }
 
