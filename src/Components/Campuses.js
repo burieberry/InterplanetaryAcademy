6f981886d5a4c1777a970a18a437b014250e9d11
Burcu Noyan <burcunoyan@gmail.com>
2017-09-27 12:24:52 -0400

    add section heads

--- src/Components/Campuses.js
+++ src/Components/Campuses.js
@@ -3,16 +3,17 @@ import React from 'react';
 
 const Campuses = ({ campuses }) => {
   return (
-    <section className="row">
-      <ul className="col-xs-10 list-unstyled">
+    <section className="col-xs-10">
+      <h2 style={{ paddingBottom: '18px' }}>Campuses</h2>
+      <ul className="list-unstyled">
         {
           campuses.map(campus => {
             return (
               <li key={ campus.id } className="col-xs-4">
                 <section className="panel panel-default">
-                  <h4 className="panel-heading" style={{ marginTop: '0' }}>
+                  <h3 className="panel-heading" style={{ marginTop: '0' }}>
                     { campus.name }
-                  </h4>
+                  </h3>
                   <div className="panel-body">
                     <img src={ campus.image } width="100%" />
                   </div>
--- src/Components/Students.js
+++ src/Components/Students.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Students = ({ students, campuses }) => {
   return (
-    <section className="col-xs-10">
+    <section className="col-xs-8">
+      <h2 style={{ paddingBottom: '18px' }}>Students</h2>
       <table className="table">
         <thead>
           <tr>
             <th>#</th>
             <th>Name</th>
+            <th>Email</th>
             <th>Campus</th>
           </tr>
         </thead>
@@ -16,6 +18,7 @@ const Students = ({ students, campuses }) => {
           students.map(student => {
             return (
               <tr key={ student.id }>
+                <td>{ student.id }</td>
                 <td><strong>{ student.name }</strong></td>
                 <td><a href={`mailTo:${ student.email }`}>{ student.email }</a></td>
                 <td>
