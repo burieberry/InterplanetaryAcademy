b59560e3d83b93481e068c7ff6ad061dd829cf2e
Burcu Noyan <burcunoyan@gmail.com>
2017-09-27 12:20:40 -0400

    add more campuses and edit students

--- db/seed.js
+++ db/seed.js
@@ -4,21 +4,24 @@ const { Student, Campus } = db.models;
 
 const seed = () => {
   return Promise.all([
-    Student.create({ name: 'Eren', email: 'eren@mh.com' }),
-    Student.create({ name: 'Mikasa', email: 'mikasa@mh.com' }),
-    Student.create({ name: 'Armin', email: 'armin@mh.com' }),
-    Student.create({ name: 'Annie', email: 'annie@mh.com' }),
-    Student.create({ name: 'Erwin', email: 'erwin@mh.com' }),
-    Campus.create({ name: 'Campus 1', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Campus 2', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Campus 3', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Campus 4', image: 'https://placeimg.com/400/250/arch' })
+    Student.create({ name: 'Eren', email: 'eren@mhacademy.com' }),
+    Student.create({ name: 'Mikasa', email: 'mikasa@mhacademy.com' }),
+    Student.create({ name: 'Armin', email: 'armin@mhacademy.com' }),
+    Student.create({ name: 'Annie', email: 'annie@mhacademy.com' }),
+    Student.create({ name: 'Erwin', email: 'erwin@mhacademy.com' }),
+    Campus.create({ name: 'Earth', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Moon', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Mars', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Pluto', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Jupiter', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Venus', image: 'https://placeimg.com/400/250/arch' })
   ])
-  .then(([ eren, mikasa, armin, annie, erwin, c1, c2, c3 ]) => {
-    c1.addStudent(eren);
-    c1.addStudent(mikasa);
-    c2.addStudent(armin);
-    c3.addStudent(erwin);
+  .then(([ s1, s2, s3, s4, s5, c1, c2, c3, c4 ]) => {
+    c1.addStudent(s1);
+    c1.addStudent(s2);
+    c2.addStudent(s3);
+    c3.addStudent(s4);
+    c4.addStudent(s5);
   })
 };
 
