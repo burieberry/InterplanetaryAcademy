19714e6fd2771256e67cba4f32aa5f3c2efb9a95
Burcu Noyan <burcunoyan@gmail.com>
2017-09-27 13:15:52 -0400

    refactor navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,16 +4,29 @@ import { Link } from 'react-router-dom';
 
 const Nav = ({ router }) => {
   const { pathname } = router.location;
+  const tabs = [
+    {
+      title: 'Campuses',
+      path: '/campuses'
+    },
+    {
+      title: 'Students',
+      path: '/students'
+    }
+  ]
 
   return (
     <nav className="col-xs-12" style={{ paddingBottom: '18px' }}>
       <ul className="nav nav-tabs">
-        <li className={ pathname === '/campuses' ? 'active' : null }>
-          <Link to="/campuses">Campuses</Link>
-        </li>
-        <li className={ pathname === '/students' ? 'active' : null }>
-          <Link to="/students">Students</Link>
-        </li>
+        {
+          tabs.map(tab => {
+            return (
+              <li key={ tab.path } className={ pathname === tab.path ? 'active' : null }>
+                <Link to={ tab.path }>{ tab.title }</Link>
+              </li>
+            )
+          })
+        }
       </ul>
     </nav>
   );
