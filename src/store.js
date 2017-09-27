ff2b79d8da2068b6cf0af2ddb0edf7428b11efcb
Burcu Noyan <burcunoyan@gmail.com>
2017-09-26 23:11:49 -0400

    get campuses

--- src/store.js
+++ src/store.js
@@ -1,8 +1,37 @@
 'use strict';
 import { createStore } from 'redux';
+import axios from 'axios';
 
-const reducer = (state, action) => {
-  return state;
+
+const initialState = {
+  campuses: []
+};
+
+const GET_CAMPUSES = 'GET_CAMPUSES';
+
+const getCampuses = (campuses) => {
+  return {
+    type: GET_CAMPUSES,
+    campuses
+  }
+};
+
+export const fetchCampuses = () => {
+  return function thunk(dispatch) {
+    return axios.get('/api/campuses')
+      .then(res => res.data)
+      .then(campuses => dispatch(getCampuses(campuses)));
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case GET_CAMPUSES:
+      return action.campuses
+
+    default:
+      return state;
+  }
 };
 
 const store = createStore(reducer);
