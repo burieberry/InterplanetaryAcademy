bc6ee87fc784d1645f0971bc9d682a652286c29f
Burcu Noyan <burcunoyan@gmail.com>
2017-09-27 12:48:04 -0400

    edit seed data

--- db/seed.js
+++ db/seed.js
@@ -4,24 +4,27 @@ const { Student, Campus } = db.models;
 
 const seed = () => {
   return Promise.all([
-    Student.create({ name: 'Eren', email: 'eren@mhacademy.com' }),
-    Student.create({ name: 'Mikasa', email: 'mikasa@mhacademy.com' }),
-    Student.create({ name: 'Armin', email: 'armin@mhacademy.com' }),
-    Student.create({ name: 'Annie', email: 'annie@mhacademy.com' }),
-    Student.create({ name: 'Erwin', email: 'erwin@mhacademy.com' }),
+    Student.create({ name: 'Rick Sanchez', email: 'ricky@mhacademy.com' }),
+    Student.create({ name: 'Morty Smith', email: 'morty@mhacademy.com' }),
+    Student.create({ name: 'King Flippy Nips', email: 'kingflippy@mhacademy.com' }),
+    Student.create({ name: 'Ma-Sha', email: 'ma-sha@mhacademy.com' }),
+    Student.create({ name: 'Purge Planet Ruler', email: 'purge@mhacademy.com' }),
+    Student.create({ name: 'Evil Rick', email: 'evilrick@mhacademy.com' }),
     Campus.create({ name: 'Earth', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Saturn', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Mars', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Venus', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Jupiter', image: 'https://placeimg.com/400/250/arch' }),
-    Campus.create({ name: 'Pluto', image: 'https://placeimg.com/400/250/arch' })
+    Campus.create({ name: 'Pluto', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Gazorpazorp', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Tiny Planet', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Hideout Planet', image: 'https://placeimg.com/400/250/arch' }),
+    Campus.create({ name: 'Purge Planet', image: 'https://placeimg.com/400/250/arch' })
   ])
-  .then(([ s1, s2, s3, s4, s5, c1, c2, c3, c4 ]) => {
+  .then(([ s1, s2, s3, s4, s5, s6, c1, c2, c3, c4, c5, c6 ]) => {
     c1.addStudent(s1);
     c1.addStudent(s2);
+    c1.addStudent(s3);
     c2.addStudent(s3);
     c3.addStudent(s4);
-    c4.addStudent(s5);
+    c5.addStudent(s6);
+    c6.addStudent(s5);
   })
 };
 
