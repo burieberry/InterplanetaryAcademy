ac098a854f9b4ae399472ff266d2ad44fe4bff2b
Burcu Noyan <burcunoyan@gmail.com>
2017-09-27 11:22:23 -0400

    link student emails and bold names

--- src/Components/Students.js
+++ src/Components/Students.js
@@ -7,13 +7,17 @@ const Students = ({ students, campuses }) => {
         students.map(student => {
           return (
             <li key={ student.id } className="list-group-item">
-              { student.name }
               <ul className="list-unstyled">
-                <li>{ student.email }</li>
+                <li>
+                  <strong>{ student.name }</strong>
+                </li>
+                <li>
+                  <a href={`mailTo:${ student.email }`}>{ student.email }</a>
+                </li>
                 <li>{
                   campuses.filter(campus => campus.id === student.campusId).length ?
                     campuses.filter(campus => campus.id === student.campusId)[0].name
-                    : 'no campus'
+                    : null
                 }</li>
               </ul>
 
