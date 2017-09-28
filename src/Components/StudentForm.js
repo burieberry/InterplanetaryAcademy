39ec46a116f4c1213f9dd5b351eecbac112c29f4
Burcu Noyan <burcunoyan@gmail.com>
2017-09-28 16:15:54 -0400

    format form

--- src/Components/StudentForm.js
+++ src/Components/StudentForm.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
+import store from '../store';
 
 class StudentForm extends Component {
   constructor() {
     super();
     this.state = {
       input: ''
-    }
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(ev) {
+    this.setState({ input: ev.target.value });
   }
 
   render() {
     const { input } = this.state;
     const { campuses } = this.props;
+    const { onChange } = this;
 
     return (
-      <section className="col-xs-8">
+      <section className="col-xs-12">
         <h2>Add Student</h2>
 
         <form>
-          <label>Name: </label>
-          <input value={ input } />
+          <div className="form-group row">
+            <label className="col-xs-3 col-form-label">Name: </label>
+            <div className="col-xs-8">
+              <input value={ input } onChange={ onChange } className="form-control" />
+            </div>
+          </div>
 
-          <label>Select Campus: </label>
-          <select>
-            {
-              campuses.map(campus => {
-                return (
-                  <option key={ campus.id }>{ campus.name }</option>
-                );
-              })
-            }
-          </select>
+          <div className="form-group row">
+            <label className="col-xs-3 col-form-label">Select Campus: </label>
+            <div className="col-xs-8">
+              <select className="form-control">
+                {
+                  campuses.map(campus => {
+                    return (
+                      <option key={ campus.id }>{ campus.name }</option>
+                    );
+                  })
+                }
+              </select>
+            </div>
+          </div>
+
+          <button className="btn btn-primary">Submit</button>
         </form>
       </section>
     );
