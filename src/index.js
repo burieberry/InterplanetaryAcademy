c0fdbd432c73959fa311aca3d425415d731b0660
Burcu Noyan <burcunoyan@gmail.com>
2017-09-29 17:03:11 -0400

    add react-redux Provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
+import store from './store';
 import App from './Components/App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={ store }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
